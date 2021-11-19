3cf9f29fb7c7ab8387ed2b677ca5f9b6a68bc5af
afaraji <alegi.bruno1@gmail.com>
2021-11-20 00:09:29 +0100

    start working on setting menu

---
+++ Assets/Scripts/SettingMenu.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingMenu : MonoBehaviour
+{
+    // https://www.youtube.com/watch?v=YOaYQrN1oYQ
+}
